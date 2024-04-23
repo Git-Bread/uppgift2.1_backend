e15c0e086ef2f4ca06905010c3a4c96d26f8da75
Max Gagner <m.gagner@hotmail.se>
2024-04-23 17:16:13 +0200

    initial setup

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.port | 3000;
 
-app.set("view engine", "ejs");
-app.use(express.static("content"));
+//app.set("view engine", "ejs");
+//app.use(express.static("content"));
+app.use(express.json);
+app.use(cors());
 var bodyParser = require('body-parser');
 
 // configure the app to use bodyParser()
@@ -19,34 +25,34 @@ app.listen(port, () => {console.log("running")});
 //sql
 const mysql = require('mysql');
 const initial = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: ''
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 initial.connect();
 
+//adds the database, probaly not needed in this case but might be nice
 initial.query("CREATE DATABASE IF NOT EXISTS Labb2Backend",function(error){if (error) {throw error;}})
 
 initial.end()
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: "Labb1Backend"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 connection.connect();
 
-//connection.query("DROP TABLE IF EXISTS Course", function(error){if (error) {throw error;}})
 connection.query("CREATE TABLE IF NOT EXISTS Jobs (id Int NOT NULL PRIMARY KEY, companyname varchar(20), jobtitle varchar(20), startdate DATE, enddate DATE)", function(error){if (error) {throw error;}})
 
 connection.query("SELECT id FROM Jobs", function(error, results) {
     if (error) {
         throw error;
     }
-    //creates base template if empty
+    //populates if empty
     if (!results[0]) {
         create();
     }
@@ -59,20 +65,9 @@ function create() {
     connection.query("INSERT Jobs VALUES (3,'Glassbolaget3','GlassGeneral','2024-01-14','CURDATE()')",function(error){if (error) {throw error;}})
 }
 
-function ask(question){
-    return new Promise((resolve, reject) => {
-        try {
-            connection.query(question, function (error, result) {  
-                if (error) {
-                    return reject(error);
-                }
-                return resolve(result);
-                })   
-        } catch (e) {
-            reject(e)
-        }
-    })
-}
+app.listen(port, () => {
+    console.log("server is running:" + port)
+});
 
 app.post("/POST",  async function name(req, res) {
     await removeEntry(req.body);
